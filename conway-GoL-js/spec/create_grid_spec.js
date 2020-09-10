5236654703805fa240c85287de3dc0c73543abc6
Ryan Viegas <rjkviegas@gmail.com>
2020-09-10 17:29:11 +0100

    create_grid returns a 0 filled 3x3 2D array when iot receives 3 and 3

--- conway-GoL-js/spec/create_grid_spec.js
+++ conway-GoL-js/spec/create_grid_spec.js
@@ -1,7 +1,10 @@
 const { create_grid } = require("../src/create_grid");
 
 describe('create_grid', function() {
-  it('returns a 2x2 2D array filled with 0s when it receives 2 and 2',function() {
+  it('returns a 0 filled 2x2 2D array when it receives 2 and 2',function() {
     expect(create_grid(2, 2)).toEqual([[0, 0], [0, 0]]);
+  });
+  it('returns a 0 filled 3x3 2D array when it receives 3 and 3',function() {
+    expect(create_grid(3, 3)).toEqual([[0, 0, 0], [0, 0, 0], [0, 0, 0]]);
   });
 });
--- conway-GoL-js/src/create_grid.js
+++ conway-GoL-js/src/create_grid.js
@@ -1,5 +1,13 @@
-const create_grid = function() {
-  return [[0, 0], [0, 0]]
+const create_grid = function(row, col) {
+  grid = [];
+  for(let i = 0; i < row; i += 1) {
+    let row = [];
+    grid.push(row);
+    for(let j = 0; j < col; j += 1) {
+      row.push(0);
+    };
+  };
+  return grid;
 };
 
 module.exports = { create_grid };
