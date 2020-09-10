6baa59b7f8bddb64767be600099a2cc07c52f1ef
Ryan Viegas <rjkviegas@gmail.com>
2020-09-10 17:30:45 +0100

    Test create_grid returns a 0 filled 2x3 2D array when it receives 2 and 3

--- conway-GoL-js/spec/create_grid_spec.js
+++ conway-GoL-js/spec/create_grid_spec.js
@@ -7,4 +7,7 @@ describe('create_grid', function() {
   it('returns a 0 filled 3x3 2D array when it receives 3 and 3',function() {
     expect(create_grid(3, 3)).toEqual([[0, 0, 0], [0, 0, 0], [0, 0, 0]]);
   });
+  it('returns a 0 filled 2x3 2D array when it receives 2 and 3',function() {
+    expect(create_grid(2, 3)).toEqual([[0, 0, 0,], [0, 0, 0]]);
+  });
 });
