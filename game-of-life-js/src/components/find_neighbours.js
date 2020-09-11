9a183cdd82bfe3b29be96b37be16b73aa321600a
Ryan Viegas <rjkviegas@gmail.com>
2020-09-11 14:37:46 +0100

    JSLint

--- game-of-life-js/src/components/find_neighbours.js
+++ game-of-life-js/src/components/find_neighbours.js
@@ -1,24 +1,30 @@
-function neighbour_finder_constructor (spec) {
-  let {grid, x, y} = spec;
-  const north = function () {
-    if (y === 0) { return undefined }
-    return [y - 1, x];
-  };
-  const south = function () {
-    if (y + 1 >= grid.length) { return undefined }
-    return [y + 1, x];
-  };
-  const east = function () {
-    if (x + 1 >= grid[y].length) { return undefined }
-    return [y, x + 1];
-  };
-  return Object.freeze({
-    north,
-    south,
-    east
-  });
+function neighbour_finder_constructor(spec) {
+    let {grid, x, y} = spec;
+    const north = function () {
+        if (y === 0) {
+            return undefined;
+        }
+        return [y - 1, x];
+    };
+    const south = function () {
+        if (y + 1 >= grid.length) {
+            return undefined;
+        }
+        return [y + 1, x];
+    };
+    const east = function () {
+        if (x + 1 >= grid[y].length) {
+            return undefined;
+        }
+        return [y, x + 1];
+    };
+    return Object.freeze({
+        north,
+        south,
+        east
+    });
 }
 
 module.exports = { 
-  neighbour_finder_constructor
+    neighbour_finder_constructor
 };
