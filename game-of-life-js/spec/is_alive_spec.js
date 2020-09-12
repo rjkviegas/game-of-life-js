681c7ab4d0c1717b7eaacf6caf60c23f4d7d0c4a
Ryan Viegas <rjkviegas@gmail.com>
2020-09-12 15:59:40 +0100

    create cell_checker_constructor with is_alive

--- game-of-life-js/spec/is_alive_spec.js
+++ game-of-life-js/spec/is_alive_spec.js
@@ -1,14 +1,26 @@
-const { is_alive } = require("../src/components/is_alive");
+const { cell_checker_constructor } = require("../src/components/is_alive");
 
-describe("is_alive", function () {
-  describe("checks if value is 1", function () {
+describe("cell_checker", function () {
+  describe("is_alive", function () {
     it("returns true when grid element is 1", function () {
-      const grid = [[1, 0], [0, 0]];
-      expect(is_alive(grid, 0, 0)).toBeTrue();
+      const spec = {
+        grid: [[1, 0],
+               [0, 0]],
+        y: 0,
+        x: 0
+        };
+      const cell_checker = cell_checker_constructor(spec);
+      expect(cell_checker.is_alive()).toBeTrue();
     });
     it("returns false when grid element is 0", function () {
-      const grid = [[0, 1], [1, 1]];
-      expect(is_alive(grid, 0, 0)).toBeFalse();
+      const spec = {
+        grid: [[0, 1],
+               [1, 1]],
+        y: 0,
+        x: 0
+      }
+      const cell_checker = cell_checker_constructor(spec);
+      expect(cell_checker.is_alive()).toBeFalse();
     });
   });
 });
--- game-of-life-js/src/components/is_alive.js
+++ game-of-life-js/src/components/is_alive.js
@@ -1,5 +1,11 @@
-const is_alive = function (grid, y, x) {
-  return grid[y][x] == 1;
+function cell_checker_constructor (spec) {
+    let { grid, y, x } = spec;
+    const is_alive = function () {
+        return grid[y][x] === 1;
+    }
+    return Object.freeze({
+      is_alive
+    })
 };
 
-module.exports = { is_alive };
+module.exports = { cell_checker_constructor };
